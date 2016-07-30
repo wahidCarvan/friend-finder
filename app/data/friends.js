6320559e852f0afbbab8fac05848038638f66a6f
wahidc7101 <wahidc7101@gmail.com>
2016-07-30 10:13:41 -0400

    added changes

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// we need to send the data to our templates
+once the user completes the survey i need modal to pop with their match 
+// friends.js  will hold our data
+// saving our data in an array of objects
+ var userData =[
+ {
+  "name":"Wahid",
+  "photo":"add a link",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+  ]
+}  
+];
+// we need to the grab the data from the objects using Jquery
